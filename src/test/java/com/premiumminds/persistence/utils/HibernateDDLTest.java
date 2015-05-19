dce11bfb88553c66ecc6eccf43f01f927f672439
Filipe Roque <froque@premium-minds.com>
2015-05-19 17:31:49 +0100

    unit tests for Hibernate*DDL

---
+++ src/test/java/com/premiumminds/persistence/utils/HibernateDDLTest.java
@@ -0,0 +1,152 @@
+package com.premiumminds.persistence.utils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintStream;
+import java.nio.file.Files;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HibernateDDLTest {
+
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	
+	@Before
+	public void setUpStreams() {
+	    System.setOut(new PrintStream(outContent));
+	}
+	
+	@After
+	public void cleanUpStreams() {
+	    System.setOut(null);
+	}
+	
+	@Test
+	public void testNoArgs() throws Exception {
+
+		String[] args = new String[]{ };
+		
+		HibernateDDL.main(args);
+		
+		assertEquals("Usage: \n" +  
+					"	--create unitName [filename] - Create table commands\n" + 
+					"	--create-drop unitName [filename] - Create table and drop commands\n" + 
+					"	--update unitName jdbcUrl jdbcUsername jdbcPassword [filename] - Alter table commands based on your database\n" + 
+					"\n" + 
+					"	[filename] is the name of the file where to write (it's optional)\n", outContent.toString());
+	}
+
+	@Test
+	public void testUpdateCommand() throws Exception {
+
+		String[] args = new String[]{   "--update", 
+										"application-data-unit-export", 
+										"jdbc:h2:" + getClass().getResource("/foobar.mv.db").getFile().replace(".mv.db", ""), 
+										"foo", 
+										"bar"};
+		
+		HibernateDDL.main(args);
+		
+		assertEquals("\n    alter table FooBar \n        add column bar integer not null;\n", outContent.toString());
+	}
+	
+	@Test
+	public void testUpdateCommandWithFile() throws Exception {
+
+		File file = File.createTempFile("updatedb", ".sql");
+		file.deleteOnExit();
+		
+		String[] args = new String[] {  "--update", 
+										"application-data-unit-export",
+										"jdbc:h2:" + getClass().getResource("/foobar.mv.db").getFile().replace(".mv.db", ""), 
+										"foo", 
+										"bar",
+										file.getAbsolutePath()};
+		
+		HibernateDDL.main(args);
+
+		assertTrue(outContent.toString().isEmpty());
+		assertEquals("\n    alter table FooBar \n        add column bar integer not null;\n", new String(Files.readAllBytes(file.toPath())));
+	}
+
+	@Test
+	public void testCreateCommand() throws Exception {
+
+		String[] args = new String[]{ "--create", "application-data-unit-export"};
+		
+		HibernateDDL.main(args);
+		
+		assertEquals("\n" +
+					"    create table FooBar (\n" +
+					"        id integer not null,\n" +
+					"        bar integer not null,\n" +
+					"        foo varchar(255),\n" +
+					"        primary key (id)\n" +
+					"    );\n", outContent.toString());
+	}
+	
+	@Test
+	public void testCreateCommandWithFile() throws Exception {
+
+		File file = File.createTempFile("updatedb", ".sql");
+		file.deleteOnExit();
+		
+		String[] args = new String[]{ "--create", "application-data-unit-export", file.getAbsolutePath()};
+		
+		HibernateDDL.main(args);
+		
+		assertTrue(outContent.toString().isEmpty());
+		assertEquals("\n" +
+					"    create table FooBar (\n" +
+					"        id integer not null,\n" +
+					"        bar integer not null,\n" +
+					"        foo varchar(255),\n" +
+					"        primary key (id)\n" +
+					"    );\n", new String(Files.readAllBytes(file.toPath())));
+	}
+	
+	@Test
+	public void testCreateDropCommand() throws Exception {
+
+		String[] args = new String[]{ "--create-drop", "application-data-unit-export"};
+		
+		HibernateDDL.main(args);
+		
+		assertEquals("\n" +
+					"    drop table FooBar if exists;\n" +
+					"\n" +
+					"    create table FooBar (\n" +
+					"        id integer not null,\n" +
+					"        bar integer not null,\n" +
+					"        foo varchar(255),\n" +
+					"        primary key (id)\n" +
+					"    );\n", outContent.toString());
+	}
+
+	@Test
+	public void testCreateDropCommandWithFile() throws Exception {
+
+		File file = File.createTempFile("updatedb", ".sql");
+		file.deleteOnExit();
+		
+		String[] args = new String[]{ "--create-drop", "application-data-unit-export", file.getAbsolutePath()};
+		
+		HibernateDDL.main(args);
+		
+		assertTrue(outContent.toString().isEmpty());
+		assertEquals("\n" +
+					"    drop table FooBar if exists;\n" +
+					"\n" +
+					"    create table FooBar (\n" +
+					"        id integer not null,\n" +
+					"        bar integer not null,\n" +
+					"        foo varchar(255),\n" +
+					"        primary key (id)\n" +
+					"    );\n", new String(Files.readAllBytes(file.toPath())));
+	}
+}
